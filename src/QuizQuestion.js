ea575c62f3b61e25def7ce440a6f5d3a5368b15d
LeiGordon <gordon.lei@xbs.ai>
2021-07-19 15:30:39 -0400

    Completed module

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,6 +8,12 @@ class QuizQuestion extends Component {
     this.state = { quiz_position: 1 };
   }
 
+  handleClick(buttonText) {
+    if (this.props.quiz_question.answer === buttonText) {
+      this.props.showNextQuestionHandler();
+    }
+  }
+
   render() {
     return (
       <main>
@@ -16,9 +22,17 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton
-              button_text={this.props.quiz_question.answer_options[0]}
-            />
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => {
+                return (
+                  <QuizQuestionButton
+                    clickHandler={this.handleClick.bind(this)}
+                    key={index}
+                    button_text={answer_option}
+                  />
+                );
+              }
+            )}
           </ul>
         </section>
       </main>
